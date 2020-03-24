e5569466bdbb157d0adb6fc6aad25b588d12677d
Linnéa <linnea@klangby.com>
2020-03-24 21:39:02 +0100

    Revert "Revert ".""
    This reverts commit 79740b311a4b913a8aed6294a2bc5e792462a8c5.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed = 3;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float controlX = Input.GetAxisRaw("Horizontal");
+        float controlY = Input.GetAxisRaw("Vertical");
+
+
+        Vector3 movementX = Vector3.right * Time.deltaTime * speed * controlX;
+        Vector3 movementY = Vector3.up * Time.deltaTime * speed * controlY;
+
+
+        transform.Translate(movementX + movementY);
+        if (Mathf.Abs(transform.position.x) > 8.5)
+        {
+            transform.Translate(-movementX);
+        }
+
+        if (Mathf.Abs(transform.position.y) > 4.5)
+        {
+            transform.Translate(-movementY);
+        }
+
+
+    }
+}
+
